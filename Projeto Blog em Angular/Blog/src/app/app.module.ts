9de1f9e7993c2680081a8b6036c012f7a469a91a
Fernando Rodrigues <nandocf20@gmail.com>
2023-04-20 03:04:31 -0300

    inicio projeto blog com Angular

---
+++ Projeto Blog em Angular/Blog/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ManuBarComponent } from './components/manu-bar/manu-bar.component';
+import { TitleComponent } from './components/title/title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ManuBarComponent,
+    TitleComponent,
+    BigCardComponent,
+    SmallCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
