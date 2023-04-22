380283d6a5f9f35b03b7f5228847d27a06b40b32
Fernando Rodrigues <nandocf20@gmail.com>
2023-04-22 11:13:00 -0300

    visual estructure of blog complete

--- Projeto Blog em Angular/Blog/src/app/app.module.ts
+++ Projeto Blog em Angular/Blog/src/app/app.module.ts
@@ -3,18 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ManuBarComponent } from './components/manu-bar/manu-bar.component';
 import { TitleComponent } from './components/title/title.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ManuBarComponent,
     TitleComponent,
     BigCardComponent,
-    SmallCardComponent
+    SmallCardComponent,
+    MenuBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
