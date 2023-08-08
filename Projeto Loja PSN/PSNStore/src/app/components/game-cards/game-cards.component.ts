93d010e1c7dcbf530b72b96e655f47f635bc5398
Fernando Rodrigues <nandocf20@gmail.com>
2023-08-08 02:47:27 -0300

    criação da sessão de cards dos jogos com grid css

--- Projeto Loja PSN/PSNStore/src/app/components/game-cards/game-cards.component.ts
+++ Projeto Loja PSN/PSNStore/src/app/components/game-cards/game-cards.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-cards.component.css']
 })
 export class GameCardsComponent implements OnInit {
+  root = '/assets/'
+  
+  gameCards: any = [
+    { id: 1, name: 'jogo1'},
+    { id: 2, name: 'jogo2'},
+    { id: 3, name: 'jogo3'},
+    { id: 4, name: 'jogo4'},
+    { id: 5, name: 'jogo5'},
+    { id: 6, name: 'jogo6'},
+    { id: 7, name: 'jogo7'},
+    { id: 8, name: 'jogo8'}
+    
+  ]
 
   constructor() { }
 
